003300cc6fa4493c68eca33d2c6d6932446886b8
Lucas Côrtes <lmattoscortes@gmail.com>
2021-06-07 20:27:35 -0300

    Create inputcolors.js

---
+++ inputcolors.js
@@ -0,0 +1,18 @@
+/* Quando desmarca o input/select/textarea e ele tá com value diferente de nada */
+/* o backgruond-color e a cor do texto mudam de acordo com o que tá definido no :root {} no styles */
+/* (ou outra cor que colocar no lugar das 'var(--cor_um)'), usando o formato #FFFFFF (hex) */
+/* Input colors */
+$('input, select, textarea').on('blur', function() {
+        if ($(this).val() === '') {
+                var color = 'var(--cor_um)';
+                var color2 = 'var(--cor_dois)';
+        } else if ($(this).val() !== '') {
+                var color = 'var(--cor_dois)';
+                var color2 = 'var(--cor_um)';
+        }
+        $(this).css({
+                'background-color': color,
+                'color': color2
+        });
+});
+/* Input colors */
