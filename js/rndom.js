56818488cc23953ebadc5b8b805f7ec72a36d06e
Lucas Côrtes <lmattoscortes@gmail.com>
2021-06-17 13:18:37 -0300

    Create rndom.js

---
+++ js/rndom.js
@@ -0,0 +1,5 @@
+/* função pra retornar um número random entre um e outro */
+function Rndm(min, max) {
+        return Math.random() * (max - min + 1) + min
+}
+/* função pra retornar um número random entre um e outro */
