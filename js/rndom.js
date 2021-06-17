44b793bb7c50655fe0c88ddcc7eccc0dc9a3e35a
Lucas Côrtes <lmattoscortes@gmail.com>
2021-06-17 13:24:35 -0300

    Update rndom.js

--- js/rndom.js
+++ js/rndom.js
@@ -1,5 +1,12 @@
 /* função pra retornar um número random entre um e outro */
+
 function Rndm(min, max) {
         return Math.random() * (max - min + 1) + min
 }
+
+// e a versão pra números inteiros
+function Rndm(min, max) {
+        return Math.round(Math.random() * (max - min + 1) + min)
+}
+
 /* função pra retornar um número random entre um e outro */
